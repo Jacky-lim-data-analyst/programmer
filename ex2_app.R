beefa3aac3cce3eb890535eaef1b5d827b58e8b1
Jacky-lim-data-analyst <84822819+Jacky-lim-data-analyst@users.noreply.github.com>
2021-06-18 11:00:23 +0800

    Add files via upload

---
+++ ex2_app.R
@@ -0,0 +1,71 @@
+# GAM model for the prediction of onset of diabetes
+library(mgcv)
+library(ggplot2)
+library(shiny)
+
+data=read.csv('diabetes.csv')
+# use all the data to train gam model, First set up the formula
+vars=setdiff(colnames(data),"Outcome")
+form=as.formula("Outcome==1~Pregnancies+s(Glucose)+BloodPressure+
+                SkinThickness+Insulin+s(BMI)+s(DiabetesPedigreeFunction)+
+                s(Age)")
+model=gam(form,data = data, family = binomial(link="logit"))
+
+ui <- fluidPage(
+  titlePanel("Prediction of onset of diabetes"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(p("Try manipulating the values of predictors below and observe how the changes
+              in variables affect the outcome of prediction (probability). Remember that in
+              the previous discussion, we had indentified", strong("Glucose, BMI and Age"),
+              "as important variables and they were placed in the left-hand panel"),
+              br(),
+              br()
+              )
+  ),
+  
+  fluidRow(
+    column(4, 
+           "features set 1",
+           numericInput("Glucose", label = "Glucose", value = median(data$Glucose), min = 1,max=199),
+           numericInput("BMI", label = "BMI", value = median(data$BMI),min=0,max=67.1),
+           numericInput("Age", label = "Age", value = median(data$Age), min = 21, max=81, step=1)
+    ),
+    column(4, 
+           "features set 2",
+           numericInput("Pregnancies", label = "Pregnancies", value = median(data$Pregnancies), min = 0, max=17, step = 1),
+           numericInput("BloodPressure", label = "Blood Pressure", value = median(data$BloodPressure), min=0, max=122, step = 1),
+           numericInput("DiabetesPedigree", label = "Diabetes Pedigree Function", value = median(data$DiabetesPedigreeFunction), 
+                        min = 0.078, max= 2.42)
+    ),
+    column(4,
+           "features set 3",
+           numericInput("Insulin", label = "Insulin", value = median(data$Insulin), min=0, max=846),
+           numericInput("SkinThickness", label = "Skin Thickness", value = median(data$SkinThickness), min=0, max=99)
+    )
+  ),
+  fluidRow(
+    column(12, plotOutput("bar"))
+  )
+)
+
+server <- function(input, output, session) {
+  df=reactive({
+    newdata=data.frame(Glucose=input$Glucose,BMI=input$BMI,Age=input$Age,
+                                           Pregnancies=input$Pregnancies,BloodPressure=input$BloodPressure,
+                                           DiabetesPedigreeFunction=input$DiabetesPedigree,
+                                           Insulin=input$Insulin,SkinThickness=input$SkinThickness)
+    pred=round(predict(model,newdata = newdata,type="response"),4)
+    data.frame(class=c("non-diabetic","diabetic"),probability=c(1-pred,pred))
+  })
+  output$bar=renderPlot({
+    ggplot(data=df(),aes(x=class,y=probability,fill=class)) +
+      geom_bar(stat="identity") +
+      geom_text(aes(label=probability),vjust=1.6,color="white",size=5) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(size = 25),axis.title = element_text(size=20))+
+      ylim(0,1)
+  })
+}
+
+shinyApp(ui, server)
